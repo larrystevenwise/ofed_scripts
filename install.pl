ebaadb9e1c0c861129587f276fa82030bf680527
Vladimir Sokolovsky <vlad@mellanox.co.il>
2008-09-07 15:10:04 +0300

    nfsrdma: add support for SLES10 SP2 and 2.6.27.
    Signed-off-by: Vladimir Sokolovsky <vlad@mellanox.co.il>

--- install.pl
+++ install.pl
@@ -1647,7 +1647,7 @@ sub set_availability
     }
 
     # NFSRDMA
-    if ($kernel =~ m/2.6.26/) {
+    if ($kernel =~ m/2.6.16.60-[0-9.]*-[A-Za-z0-9.]*|2.6.2[6-7]/) {
             $kernel_modules_info{'nfsrdma'}{'available'} = 1;
     }
 
