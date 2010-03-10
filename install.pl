db9fe53430e3c08cb832a837869f8c1a1adaf22e
Vladimir Sokolovsky <vlad@mellanox.co.il>
2010-03-10 17:28:01 +0200

    install.pl: perftest depends on libibumad
    Signed-off-by: Vladimir Sokolovsky <vlad@mellanox.co.il>

--- install.pl
+++ install.pl
@@ -832,8 +832,8 @@ my %packages_info = (
             { name => "perftest", parent => "perftest",
             selected => 0, installed => 0, rpm_exist => 0, rpm_exist32 => 0,
             available => 1, mode => "user", dist_req_build => [],
-            dist_req_inst => [], ofa_req_build => ["libibverbs-devel", "librdmacm-devel"],
-            ofa_req_inst => ["libibverbs", "librdmacm"],
+            dist_req_inst => [], ofa_req_build => ["libibverbs-devel", "librdmacm-devel", "libibumad-devel"],
+            ofa_req_inst => ["libibverbs", "librdmacm", "libibumad"],
             install32 => 0, exception => 0, configure_options => '' },
         'perftest-debuginfo' =>
             { name => "perftest-debuginfo", parent => "perftest",
