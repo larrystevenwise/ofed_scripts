ecb96400cafa3a6a954164353ac29a4d5b414f08
Avneesh Pant <avneesh.pant@qlogic.com>
2010-06-06 09:42:02 +0300

    install.pl: Update the list of supported platforms by infinipath-psm
    Signed-off-by: Avneesh Pant <avneesh.pant@qlogic.com>

--- install.pl
+++ install.pl
@@ -1336,13 +1336,13 @@ my %packages_info = (
         'infinipath-psm' =>
             { name => "infinipath-psm", parent=> "infinipath-psm",
              selected => 0, installed => 0, rpm_exits => 0, rpm_exists32 => 0,
-             available => 1, mode => "user", dist_req_build => [],
+             available => 0, mode => "user", dist_req_build => [],
              dist_req_inst => [], ofa_req_build => [],
              ofa_req_inst => [], install32 => 0, exception => 0 },
         'infinipath-psm-devel' =>
             { name => "infinipath-psm-devel", parent=> "infinipath-psm",
              selected => 0, installed => 0, rpm_exits => 0, rpm_exists32 => 0,
-             available => 1, mode => "user", dist_req_build => [],
+             available => 0, mode => "user", dist_req_build => [],
              dist_req_inst => [], ofa_req_build => [],
              ofa_req_inst => ["infinipath-psm"], install32 => 0, exception => 0 },
         );
@@ -1703,14 +1703,15 @@ sub set_availability
     #         $kernel =~ m/2.6.16.[0-9.]*-[0-9.]*-[A-Za-z0-9.]*|2.6.1[7-9]|2.6.2[0-9]/) or
     #    ($arch =~ m/x86_64/ and
     #         $kernel =~ m/2.6.9-42|2.6.9-55|2.6.9-67|2.6.9-78|2.6.16.[0-9.]*-[0-9.]*-[A-Za-z0-9.]*|2.6.1[7-9]|2.6.2[0-9]/) ) {
-    if ($arch =~ m/x86_64/ and
-	     $kernel =~ m/2\.6\.9-(67|78|89)|2\.6\.18-8[4-9]\..*el5|2\.6\.18-9[0-9]\..*el5|2\.6\.18-1[0-9][0-9]\..*el5|2\.6\.16\.[6-9][0-9]-.*-.*|2\.6\.27\..*-.*/) {
-            $kernel_modules_info{'qib'}{'available'} = 1;
-            $packages_info{'libipathverbs'}{'available'} = 1;
-            $packages_info{'libipathverbs-devel'}{'available'} = 1;
-            $packages_info{'libipathverbs-debuginfo'}{'available'} = 1;
-            $packages_info{'InfiniPath-PSM'}{'available'} = 1;
-            $packages_info{'InfiniPath-PSM-devel'}{'available'} = 1;
+    if ($arch =~ m/x86_64/) {
+	    $packages_info{'infinipath-psm'}{'available'} = 1;
+	    $packages_info{'infinipath-psm-devel'}{'available'} = 1;
+	    if ($kernel =~ m/2\.6\.9-(67|78|89)|2\.6\.18-8[4-9]\..*el5|2\.6\.18-9[0-9]\..*el5|2\.6\.18-1[0-9][0-9]\..*el5|2\.6\.16\.[6-9][0-9]-.*-.*|2\.6\.27\..*-.*/) {
+		    $kernel_modules_info{'qib'}{'available'} = 1;
+		    $packages_info{'libipathverbs'}{'available'} = 1;
+		    $packages_info{'libipathverbs-devel'}{'available'} = 1;
+		    $packages_info{'libipathverbs-debuginfo'}{'available'} = 1;
+	    }
     }
 
     # Iser
