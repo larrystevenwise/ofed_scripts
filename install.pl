27043a620ce5a9904b96e277812604f27f2c09f4
Vladimir Sokolovsky <vlad@mellanox.com>
2015-03-30 19:22:36 +0300

    install.pl: Disable nfsrdma on RHEL6 and kernel 3.18
    NFSRDMA is temporary disabled due to compilation issues:
    2523, 2524, 2525
    
    Signed-off-by: Vladimir Sokolovsky <vlad@mellanox.com>

--- install.pl
+++ install.pl
@@ -2029,6 +2029,9 @@ sub set_availability
             $packages_info{'qlvnictools-debuginfo'}{'available'} = 1;
     }
 
+    if ($kernel =~ m/el6|^3\.18/) {
+            $kernel_modules_info{'nfsrdma'}{'available'} = 0;
+    }
 
     # mvapich, mvapich2 and openmpi
     if ($gcc{'gcc'}) {
